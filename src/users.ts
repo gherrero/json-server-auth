b5241374209e4e8955aa90588458e2c30237b16f
Daniel Gent <dan@whalar.com>
2020-08-06 14:43:20 +0200

    hardcoding user roles

--- src/users.ts
+++ src/users.ts
@@ -122,7 +122,7 @@ const login: Handler = (req, res, next) => {
 
 			return new Promise<string>((resolve, reject) => {
 				jwt.sign(
-					{ email },
+					{ email, realm_access: { roles: ['sonar-creator'] } },
 					JWT_SECRET_KEY,
 					{ expiresIn: JWT_EXPIRES_IN, subject: String(user.id) },
 					(error, idToken) => {
